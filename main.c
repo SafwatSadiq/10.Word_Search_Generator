ca356c1a30d347b09f7517c0ca55ef0403e21183
Safwat Sadiq <Safwatsadiq14@gmail.com>
2025-02-12 19:04:28 +0600

    Made the grid and printed it.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+    char grid[10][10];
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            grid[i][j] = '0';
+        }
+    }
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            printf("%c", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
