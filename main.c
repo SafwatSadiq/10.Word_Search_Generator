6b63e7ce47bec396cbf0abac9d55738272ec5cbc
Safwat Sadiq <Safwatsadiq14@gmail.com>
2025-02-14 11:45:27 +0600

    Created some strings and added them to the Grid.

--- main.c
+++ main.c
@@ -23,6 +23,21 @@ void gridPrinter(){
 }
 
 void main(){
+    char word1[10] = "Hello", word2[10] = "World";
     gridMaker();
+
+    for(int i=0; i<1; i++){
+        for(int j=0; j<5; j++){
+            grid[i][j] = word1[j];
+        }
+    }
+    for(int i=1; i<6; i++){
+        for(int j=0; j<1; j++){
+            grid[i][j] = word2[i-1];
+        }
+    }
+
+
+
     gridPrinter();
 }
