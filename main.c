3db81ffe85026b9bd7a1351925f198e828351243
Safwat Sadiq <Safwatsadiq14@gmail.com>
2025-03-05 15:45:29 +0600

    Maded the array a array of pointers so it is more memory efficient.

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ void placeDiagonal(char *word, int row, int column){
 }
 
 void main(){
-    char word[3][10] = {"HELLO", "WORLD", "PROJECT"};
+    char *word[3] = {"HELLO", "WORLD", "PROJECT"};
 
     gridMaker();
     
